6c7dc0838e3df73fbd5a8deeb86741b673c5e4d9
prathyushars <143388421+prathyushars@users.noreply.github.com>
2023-08-28 13:33:42 +0530

    Add files via upload

---
+++ Hyperlinks.java
@@ -0,0 +1,37 @@
+package demo;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Hyperlinks {
+    ChromeDriver driver;
+    public Hyperlinks(){
+        System.out.println("Constructor: TestCases");
+        WebDriverManager.chromedriver().timeout(30).setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    public void endTest()
+    {
+        System.out.println("End Test: TestCases");
+        driver.close();
+        driver.quit();
+
+    }
+
+    public  void countHyperlinks(){
+        driver.get("https://in.bookmyshow.com/explore/home/chennai");
+        List<WebElement> hyperlinks = driver.findElements(By .tagName("a"));
+        System.out.println(hyperlinks.size());
+
+        
+    }
+}
